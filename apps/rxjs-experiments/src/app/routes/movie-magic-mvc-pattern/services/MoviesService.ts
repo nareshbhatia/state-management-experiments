73c8c42822e3d1c0108722e1cae5eaafbbef0438
Naresh Bhatia <me@nareshbhatia.dev>
2025-02-02 12:56:28 -0500

    chore: replace from(fetch()) with fromFetch()

--- apps/rxjs-experiments/src/app/routes/movie-magic-mvc-pattern/services/MoviesService.ts
+++ apps/rxjs-experiments/src/app/routes/movie-magic-mvc-pattern/services/MoviesService.ts
@@ -2,16 +2,13 @@ import type { IMovieService } from './IMovieService';
 import type { MoviePagination } from '../../../../models';
 import type { AsyncModel } from '../packages/async-model';
 import type { Observable } from 'rxjs';
-import { from, switchMap, map, catchError, startWith } from 'rxjs';
+import { from, map, catchError, startWith } from 'rxjs';
+import { fromFetch } from 'rxjs/fetch';
 
 function fetchMovies(url: string): Observable<AsyncModel<MoviePagination>> {
-  return from(fetch(url)).pipe(
-    switchMap(async (response) => {
-      if (response.ok) {
-        return response.json() as Promise<MoviePagination>;
-      }
-      throw new Error(`Error ${response.status}`);
-    }),
+  return fromFetch(url, {
+    selector: async (response) => response.json() as Promise<MoviePagination>,
+  }).pipe(
     map((data) => ({ status: 'loaded', data }) as AsyncModel<MoviePagination>),
     catchError((error: unknown) =>
       from([{ status: 'error', error } as AsyncModel<MoviePagination>]),
